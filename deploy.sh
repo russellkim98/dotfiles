e52d94e36a4f893e93c6faf61f4885e7575f125d
Russell Kim <russellkim98@gmail.com>
2023-08-27 05:17:24 +0200

    solid tools

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,27 @@ curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 brew install git                                                                                      
 brew install zsh 
 
+# Get the current path
+SCRIPT_DIR="${0:A:h}"
+cd "${SCRIPT_DIR}"
+
+
+# Env management, priority no. 1
 brew install pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
 brew install goenv
+export GOENV_ROOT="$HOME/.goenv"
+export PATH="$GOENV_ROOT/bin:$PATH"
+
 brew install rbenv
+export RBENV_ROOT="$HOME/.rbenv"
+export PATH="$RBENV_ROOT/bin:$PATH"
+
 brew install jenv
+export JENV_ROOT="$HOME/.jenv"
+export PATH="$JENV_ROOT/bin:$PATH"
 
 
 # personal tools
