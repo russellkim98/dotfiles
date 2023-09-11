2c910c9d1725de5e92763f6fde626d7b921a1bd8
Russell Kim <russellkim98@gmail.com>
2023-09-11 15:11:09 +0200

    small changes to bash

--- deploy.sh
+++ deploy.sh
@@ -9,19 +9,16 @@ xcode-select --install
 # install brew
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
 
-
 # https://formulae.brew.sh/formula/git
-brew install git                                                                                      
-brew install zsh 
+brew install git
+brew install zsh
 
 # Get the current path
 SCRIPT_DIR="${0:A:h}"
-cd "${SCRIPT_DIR}"
-
+cd "${SCRIPT_DIR}" || exit
 
 # Env management, priority no. 1
 brew install -q pyenv, goenv, rbenv, jenv
-
 
 # personal tools
 brew install --cask iterm2
@@ -30,4 +27,3 @@ brew install neovim
 
 # poetry
 curl -sSL https://install.python-poetry.org | python3 -
-
