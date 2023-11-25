ab38d18ef7651ea796b3a37a474019b547f275de
Russell Kim <russellkim98@gmail.com>
2023-11-25 16:36:35 +0900

    new edits and plugins

--- zsh/rc.d/20_syntax_highlight.zsh
+++ zsh/rc.d/20_syntax_highlight.zsh
@@ -3,5 +3,5 @@ source "${ZDOTDIR}/plugins/syntax-highlighting/zsh-syntax-highlighting.zsh"
 ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets regexp cursor)
 # Highlight known abbrevations
 typeset -A ZSH_HIGHLIGHT_REGEXP
-ZSH_HIGHLIGHT_REGEXP+=('^[[:blank:][:space:]]*('${(j:|:)${(Qk)ABBR_REGULAR_USER_ABBREVIATIONS}}')$' 'fg=blue')
+# ZSH_HIGHLIGHT_REGEXP+=('^[[:blank:][:space:]]*('${(j:|:)${(Qk)ABBR_REGULAR_USER_ABBREVIATIONS}}')$' 'fg=blue')
 
