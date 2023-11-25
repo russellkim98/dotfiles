3679a7d9754d33f9f118d6a242531439cf2cdb49
Russell Kim <russellkim98@gmail.com>
2023-11-25 18:18:49 +0900

    fix a bit of prompting

--- zsh/rc.d/06_spaceship.zsh
+++ zsh/rc.d/06_spaceship.zsh
@@ -1,1 +1,5 @@
 source /usr/local/opt/spaceship/spaceship.zsh
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
--- zsh/rc.d/14_iterm2.zsh
+++ zsh/rc.d/14_iterm2.zsh
@@ -1,6 +1,6 @@
 # iTerm2 integration
-if [[ -v ITERM_PROFILE ]] || [[ -v ITERM_SESSION ]]; then
-    source "${ZDOTDIR}/plugins/iTerm2-shell-integration/shell_integration/zsh"
-    path=(${ZDOTDIR}/plugins/iTerm2-shell-integration/utilities $path)
-fi
+# if [[ -v ITERM_PROFILE ]] || [[ -v ITERM_SESSION ]]; then
+#     source "${ZDOTDIR}/plugins/iTerm2-shell-integration/shell_integration/zsh"
+#     path=(${ZDOTDIR}/plugins/iTerm2-shell-integration/utilities $path)
+# fi
 
