5ce24ae620ea3042e39162ca51a6a66d4d4d4cad
Russell Kim <russellkim98@gmail.com>
2023-11-25 20:31:53 +0900

    fix paths

--- zsh/rc.d/08_env.zsh
+++ zsh/rc.d/08_env.zsh
@@ -3,24 +3,24 @@
 # Brew-related environments
 
 # 3.10.0 should be the global as of Aug 26, 2023
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
 pyenv init - >/dev/null
 
-export GOENV_ROOT="$HOME/.goenv"
-export PATH="$GOENV_ROOT/bin:$PATH"
+# export GOENV_ROOT="$HOME/.goenv"
+# export PATH="$GOENV_ROOT/bin:$PATH"
 goenv init - >/dev/null
 
-export PATH="$GOROOT/bin:$PATH"
-export PATH="$PATH:$GOPATH/bin"
-
-export RBENV_ROOT="$HOME/.rbenv"
-export PATH="$RBENV_ROOT/bin:$PATH"
+# export PATH="$GOROOT/bin:$PATH"
+# export PATH="$PATH:$GOPATH/bin"
+#
+# export RBENV_ROOT="$HOME/.rbenv"
+# export PATH="$RBENV_ROOT/bin:$PATH"
 rbenv init - >/dev/null
 
-export JENV_ROOT="$HOME/.jenv"
-export PATH="$JENV_ROOT/bin:$PATH"
+# export JENV_ROOT="$HOME/.jenv"
+# export PATH="$JENV_ROOT/bin:$PATH"
 jenv init - >/dev/null
 
-export POETRY_HOME="$HOME/.poetry"
-export PATH="$POETRY_HOME/bin:$PATH"
+# export POETRY_HOME="$HOME/.poetry"
+# export PATH="$POETRY_HOME/bin:$PATH"
