221b7392e945e42671ba387be7e2e2bdddeeed6f
Russell Kim <russellkim@Russells-MacBook-Pro-M3.local>
2024-03-31 19:59:23 +0900

    new changes

--- bin/install_fzf.sh
+++ bin/install_fzf.sh
@@ -5,8 +5,14 @@ install_fzf() {
 	echo "Installing fzf..."
 	pushd tools/fzf
 	if ./install --bin &>/dev/null; then
+
+		mkdir -p "${HOME}/.local/bin/fzf"
 		zf_ln -sf "${DOTFILES}/tools/fzf/bin/fzf" "${HOME}/.local/bin/fzf"
+		
+		mkdir -p "${HOME}/.local/bin/fzf-tmux"
 		zf_ln -sf "${DOTFILES}/tools/fzf/bin/fzf-tmux" "${HOME}/.local/bin/fzf-tmux"
+		
+		mkdir -p "${XDG_DATA_HOME}/man/man1"
 		zf_ln -sf "${DOTFILES}/tools/fzf/man/man1/fzf.1" "${XDG_DATA_HOME}/man/man1/fzf.1"
 		zf_ln -sf "${DOTFILES}/tools/fzf/man/man1/fzf-tmux.1" "${XDG_DATA_HOME}/man/man1/fzf-tmux.1"
 		echo "  ...done"
