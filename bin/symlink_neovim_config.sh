04f83d21e4f234dc790a0a306dd6bdec7ca405b6
Russell Kim <russellkim98@gmail.com>
2024-04-06 12:37:16 +0900

    change to neovim config

--- bin/symlink_neovim_config.sh
+++ bin/symlink_neovim_config.sh
@@ -2,9 +2,7 @@
 set -e
 # Function to symlink Neovim config
 symlink_neovim_config() {
-	local ASTRONVIM="$(git submodule | grep 'AstroNvim' | awk '{print $2}')"
-	local ASTRONVIM_CONFIG="$(git submodule | grep 'my-astronvim-config' | awk '{print $2}')"
+	local ASTRONVIM="$(git submodule | grep 'astronvim' | awk '{print $2}')"
 
 	zf_ln -snf "${DOTFILES}/${ASTRONVIM}" "${XDG_CONFIG_HOME}/nvim"
-	zf_ln -snf "${DOTFILES}/${ASTRONVIM_CONFIG}" "${XDG_CONFIG_HOME}/nvim/lua/user"
 }
